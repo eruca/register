31e37881d4517e2e74fde44ef140d7982d0023bf
eruca <nickwill1984@126.com>
2019-08-02 10:35:24 +0800

    完善页面及图片

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
-import "@tarojs/async-await";
-import Taro, { Component, Config } from "@tarojs/taro";
-import { Provider } from "@tarojs/redux";
+import '@tarojs/async-await';
+import Taro, { Component, Config } from '@tarojs/taro';
+import { Provider } from '@tarojs/redux';
 
-import Index from "./pages/index";
+import Index from './pages/index';
 
-import configStore from "./store";
+import configStore from './store';
 
-import "./app.scss";
+import './app.scss';
 
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
@@ -17,61 +17,78 @@ import "./app.scss";
 const store = configStore();
 
 class App extends Component {
-  /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
-  config: Config = {
-    pages: ["pages/index/index", "pages/patient/index"],
+    /**
+     * 指定config的类型声明为: Taro.Config
+     *
+     * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+     * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+     * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+     */
+    config: Config = {
+        pages: [
+            'pages/index/index',
+            'pages/patient/index',
+            'pages/result/index',
+            'pages/form/index',
+        ],
 
-    tabBar: {
-      list: [
-        {
-          pagePath: "pages/index/index",
-          text: "登记",
-          iconPath: "./assets/form.png",
-          selectedIconPath: "./assets/form_selected.png"
+        tabBar: {
+            list: [
+                {
+                    pagePath: 'pages/index/index',
+                    text: '首页',
+                    iconPath: './assets/home.png',
+                    selectedIconPath: './assets/home_selected.png',
+                },
+                {
+                    pagePath: 'pages/patient/index',
+                    text: '患者',
+                    iconPath: './assets/user.png',
+                    selectedIconPath: './assets/user_selected.png',
+                },
+                {
+                    pagePath: 'pages/form/index',
+                    text: '登记',
+                    iconPath: './assets/form.png',
+                    selectedIconPath: './assets/form_selected.png',
+                },
+                {
+                    pagePath: 'pages/result/index',
+                    text: '结局',
+                    iconPath: './assets/result.png',
+                    selectedIconPath: './assets/result_selected.png',
+                },
+            ],
+            color: '#a6a6a6',
+            selectedColor: '#78a4fa',
+            backgroundColor: '#ffffff',
+            borderStyle: 'black',
         },
-        {
-          pagePath: "pages/patient/index",
-          text: "患者",
-          iconPath: "./assets/user.png",
-          selectedIconPath: "./assets/user_selected.png"
-        }
-      ],
-      color: "#a6a6a6",
-      selectedColor: "#78a4fa",
-      backgroundColor: "#ffffff",
-      borderStyle: "black"
-    },
-    window: {
-      backgroundTextStyle: "light",
-      navigationBarBackgroundColor: "#fff",
-      navigationBarTitleText: "WeChat",
-      navigationBarTextStyle: "black"
+        window: {
+            backgroundTextStyle: 'light',
+            navigationBarBackgroundColor: '#fff',
+            navigationBarTitleText: 'WeChat',
+            navigationBarTextStyle: 'black',
+        },
+    };
+
+    componentDidMount() {}
+
+    componentDidShow() {}
+
+    componentDidHide() {}
+
+    componentDidCatchError() {}
+
+    // 在 App 类中的 render() 函数没有实际作用
+    // 请勿修改此函数
+    render() {
+        return (
+            <Provider store={store}>
+                <Index />
+            </Provider>
+        );
     }
-  };
-
-  componentDidMount() {}
-
-  componentDidShow() {}
-
-  componentDidHide() {}
-
-  componentDidCatchError() {}
-
-  // 在 App 类中的 render() 函数没有实际作用
-  // 请勿修改此函数
-  render() {
-    return (
-      <Provider store={store}>
-        <Index />
-      </Provider>
-    );
-  }
 }
 
-Taro.render(<App />, document.getElementById("app"));
+Taro.render(<App />, document.getElementById('app'));
