e943c0d9a734735f36d771034384c1330e0b5c65
eruca <nickwill1984@126.com>
2019-09-19 23:47:20 +0800

    增加营养配方的统计

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -36,6 +36,7 @@ export default function Result() {
     const [years, setYears] = useState(origin);
     const [group, setGroup] = useState([]);
     const [mygroup, setMygroup] = useState(new Map());
+    const [nutrients, setNutrients] = useState(new Map());
 
     useEffect(() => {
         Taro.cloud.callFunction({
@@ -81,10 +82,39 @@ export default function Result() {
 
                 const map = (res as any).result.mygroup.list.reduce((acc, currValue) => {
                     acc.set(currValue['_id'], currValue['num']);
-                    acc.set('sum', acc.get('sum') + currValue['num']);
+                    acc.set('sum', (acc.get('sum') || 0) + currValue['num']);
                     return acc;
-                }, new Map([['sum', 0]]));
+                }, new Map());
                 setMygroup(map);
+
+                const nutrientMap = new Map();
+                (res as any).result.nutrients.list.forEach(item => {
+                    if (item._id === false || item._id === null) {
+                        nutrientMap.set(
+                            'peptide-0',
+                            (nutrientMap.get('peptide-0') || 0) + item.num
+                        );
+                    } else {
+                        nutrientMap.set(
+                            'peptide-1',
+                            (nutrientMap.get('peptide-1') || 0) + item.num
+                        );
+                    }
+                });
+                (res as any).result.mynutrients.list.forEach(item => {
+                    if (item._id === false || item._id === null) {
+                        nutrientMap.set(
+                            'mypeptide-0',
+                            (nutrientMap.get('mypeptide-0') || 0) + item.num
+                        );
+                    } else {
+                        nutrientMap.set(
+                            'mypeptide-1',
+                            (nutrientMap.get('mypeptide-1') || 0) + item.num
+                        );
+                    }
+                });
+                setNutrients(nutrientMap);
             },
             fail: console.error,
         });
@@ -104,7 +134,7 @@ export default function Result() {
                 </View>
             </Picker>
 
-            <AtCard title="完成情况统计">
+            <AtCard title="按完成情况">
                 <View className="at-row at-row__justify--center">
                     <View className="at-col at-col-6">已达1周</View>
                     <View className="at-col at-col-6">已有结果</View>
@@ -121,29 +151,58 @@ export default function Result() {
                 )}
             </AtCard>
 
-            <View style={{ marginTop: '5PX' }}>
-                <AtCard title="按病种">
+            <View style={{ margin: '5PX 0' }}>
+                <AtCard title="按营养配方">
                     <View className="at-row at-row__justify--center" style={{ color: '#6699FF' }}>
-                        <View className="at-col at-col-6">病种</View>
+                        <View className="at-col at-col-6">配方</View>
                         <View className="at-col at-col-3">我的</View>
                         <View className="at-col at-col-3">全部</View>
                     </View>
-                    {group.map(item => (
-                        <View className="at-row at-row__justify--center">
-                            <View className="at-col at-col-6">{selector[item['_id']]}</View>
-                            <View className="at-col at-col-3">{mygroup.get(item['_id']) || 0}</View>
-                            <View className="at-col at-col-3">{item['num']}</View>
-                        </View>
-                    ))}
+                    <View className="at-row at-row__justify--center">
+                        <View className="at-col at-col-6">短肽</View>
+                        <View className="at-col at-col-3">{`${nutrients.get('mypeptide-1') ||
+                            0}`}</View>
+                        <View className="at-col at-col-3">{`${nutrients.get('peptide-1') ||
+                            0}`}</View>
+                    </View>
+                    <View className="at-row at-row__justify--center">
+                        <View className="at-col at-col-6">整蛋白</View>
+                        <View className="at-col at-col-3">{`${nutrients.get('mypeptide-0') ||
+                            0}`}</View>
+                        <View className="at-col at-col-3">{`${nutrients.get('peptide-0') ||
+                            0}`}</View>
+                    </View>
                     <View className="at-row at-row__justify--center" style={{ color: '#FF9900' }}>
                         <View className="at-col at-col-6">总计</View>
-                        <View className="at-col at-col-3">{mygroup.get('sum')}</View>
+                        <View className="at-col at-col-3">{mygroup.get('sum') || 0}</View>
                         <View className="at-col at-col-3">
                             {group.reduce((acc, curr: any) => acc + curr.num, 0)}
                         </View>
                     </View>
                 </AtCard>
             </View>
+
+            <AtCard title="按病种">
+                <View className="at-row at-row__justify--center" style={{ color: '#6699FF' }}>
+                    <View className="at-col at-col-6">病种</View>
+                    <View className="at-col at-col-3">我的</View>
+                    <View className="at-col at-col-3">全部</View>
+                </View>
+                {group.map(item => (
+                    <View className="at-row at-row__justify--center">
+                        <View className="at-col at-col-6">{selector[item['_id']]}</View>
+                        <View className="at-col at-col-3">{mygroup.get(item['_id']) || 0}</View>
+                        <View className="at-col at-col-3">{item['num']}</View>
+                    </View>
+                ))}
+                <View className="at-row at-row__justify--center" style={{ color: '#FF9900' }}>
+                    <View className="at-col at-col-6">总计</View>
+                    <View className="at-col at-col-3">{mygroup.get('sum') || 0}</View>
+                    <View className="at-col at-col-3">
+                        {group.reduce((acc, curr: any) => acc + curr.num, 0)}
+                    </View>
+                </View>
+            </AtCard>
 
             <View style={{ margin: '5PX 14PX 0 14PX' }}>
                 <AtButton
