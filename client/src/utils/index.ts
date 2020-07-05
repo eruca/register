d35084b6477272ebfcb468260b2df949c8632177
eruca <nickwill1984@126.com>
2020-07-05 23:19:24 +0800

    增加debounce，但是还没有使用

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -43,3 +43,29 @@
 //     }
 //     return true;
 // }
+
+// function debounce(func, wait, immediate) {
+//     var timeout, result;
+
+//     var debounced = function () {
+//         var context = this;
+//         var args = arguments;
+//         if (timeout) clearTimeout(timeout);
+
+//         var later = function () {
+//             timeout = null;
+//             if (!immediate) result = func.apply(context, args);
+//         };
+
+//         var callNow = immediate && !timeout;
+//         timeout = setTimeout(later, wait);
+//         if (callNow) result = func.apply(this, args);
+
+//         return result;
+//     };
+//     // debounced.cancel = function () {
+//     //     clearTimeout(timeout);
+//     //     timeout = null;
+//     // };
+//     return debounced;
+// }
